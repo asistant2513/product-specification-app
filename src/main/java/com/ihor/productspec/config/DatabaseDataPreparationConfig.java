48e01b4c500a56a19735ca5b6e8ac1b5012b329a
Ihor Larchenko <asistant2507ua@gmail.com>
2022-10-25 20:55:58 +0300

    Resource reading enhancements

--- src/main/java/com/ihor/productspec/config/DatabaseDataPreparationConfig.java
+++ src/main/java/com/ihor/productspec/config/DatabaseDataPreparationConfig.java
@@ -1,16 +1,17 @@
 package com.ihor.productspec.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Base64;
 
 @Component
 @Slf4j
@@ -23,12 +24,23 @@ public final class DatabaseDataPreparationConfig {
 
         try (Connection c = connection.getConnection()) {
             for (var script : sqlScripts) {
-                String query = new String(script.getInputStream().readAllBytes());
-                c.createStatement().execute(query);
+                String query = readInputStream(script);
+                if (!query.equals(Strings.EMPTY)) {
+                    c.createStatement().execute(query);
+                }
             }
         }
         catch (SQLException exception) {
             log.error("Error during SQL connection : {}", exception.getMessage());
+        }
+    }
+
+    private String readInputStream(final Resource resource) {
+        try (InputStream is = resource.getInputStream()) {
+            return new String(is.readAllBytes());
+        } catch (IOException e) {
+            log.error("IO exception while reading resource '{}': {}", resource.getFilename(), e.getMessage());
+            return Strings.EMPTY;
         }
     }
 }

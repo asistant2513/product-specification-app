505fea57fa8b25a5376c8138d8d4a4ed4344f3b6
Ihor Larchenko <asistant2507ua@gmail.com>
2022-10-25 20:44:56 +0300

    Added data preparation for sqlite DB

--- src/main/java/com/ihor/productspec/ProductSpecApplication.java
+++ src/main/java/com/ihor/productspec/ProductSpecApplication.java
@@ -1,14 +1,27 @@
 package com.ihor.productspec;
 
+import com.ihor.productspec.config.DatabaseDataPreparationConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.io.IOException;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@Slf4j
 public class ProductSpecApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductSpecApplication.class, args);
+		var context = SpringApplication.run(ProductSpecApplication.class, args);
+		try {
+			boolean isDBReady = Boolean.getBoolean(context.getEnvironment().getProperty("datasource.isReady"));
+			if (!isDBReady) {
+				context.getBean(DatabaseDataPreparationConfig.class).prepareDatabase(context);
+			}
+		} catch (IOException exception) {
+			log.error("Error while preparing database: {}", exception.getMessage());
+		}
 	}
 
 }
